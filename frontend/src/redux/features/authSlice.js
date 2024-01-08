fa7620a1f170651642614e07c9e3fad33a666eff
JugalSoni <sonijugal2002@gmail.com>
2024-01-09 00:04:35 +0530

    started with frontend

---
+++ frontend/src/redux/features/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    isLoggedIn: false
+}
+
+const authSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = authSlice.actions
+
+export default authSlice.reducer
